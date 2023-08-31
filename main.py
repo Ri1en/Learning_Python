7f7332ce55ba7a86a30b1becf591191ed14a26e4
Ri1en <alexandrteluhin@gmail.com>
2023-08-31 15:57:38 +0300

    Добавил валидацию атрибута method.Создал исключения, которые вызываются в методах валидации.Создал новый класс Request, унаследованный от базового класса BaseRequest. В дочернем классе создал метод валидирующий атрибут params

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+class MethodError(Exception):
+    def __init__(self, *args):
+        self.message = args[0] if args else None
+
+    def __str__(self):
+        return f"[Error INFO]: {self.message}"
+
+
+class ParamsError(Exception):
+    def __init__(self, *args):
+        self.message = args[0] if args else None
+
+    def __str__(self):
+        return f"[Error INFO]: {self.message}"
+
+
 class BaseRequest:
     def __init__(self, url, method, params=None, body=None):
         self.url = url
-        try:
-            self.method = method
-            if self.method != ("GET" or "POST"):
-                raise Exception
-        except Exception as _ex:
-            self.method = None
-            print("Unknown method")
+        self.method = method
         self.params = params
         self.body = body
+        self.is_valid_method()
 
-    def get_url(self):
-        return self.url
+    def is_valid_method(self):
+        if self.method != ("GET" or "POST"):
+            raise MethodError("The request method can only be GET or POST")
+        if self.body and self.method == "GET":
+            raise MethodError("The request method GET can't take attribute body")
 
-    def get_method(self):
-        return self.method
 
-    def get_params(self):
-        return self.params
+class Request(BaseRequest):
 
-    def get_body(self):
-        return self.body
+    def __init__(self, url, method, params=None, body=None):
+        super().__init__(url, method, params, body)
+        self.is_valid_params()
+
+    def is_valid_params(self):
+        try:
+            if len(list(self.params)) > 5:
+                raise ParamsError("object of class Request can take 5 or less params")
+        except TypeError:
+            print("Attribute params only dict")
 
 
 if __name__ == '__main__':
-    request = BaseRequest("http://url.com", "GET", {'q': 1, 'd': 2})
+    request = Request("http://url.com", "GET", {'q': 1, 'd': 2})
