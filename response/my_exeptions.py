c3a2995b70f71758e0828014375db2c295e13fd8
Ri1en <alexandrteluhin@gmail.com>
2023-09-04 19:34:55 +0300

    1

--- response/my_exeptions.py
+++
@@ -1,6 +0,0 @@
-class TimeOutError(Exception):
-    def __init__(self, *args) -> None:
-        self.message = args[0] if args else None
-
-    def __str__(self) -> str:
-        return f"[Error INFO]: {self.message}"
