ff253a8177eb50b18604740174a6dfeae3e510e6
Ri1en <114809949+Ri1en@users.noreply.github.com>
2023-10-02 19:52:12 +0300

    Delete Get_weather directory
    deleting an unused directory

--- Get_weather/__init__.py
+++
@@ -1,4 +0,0 @@
-from get_weather.postgres_db import PostgresDb
-from get_weather.db_config import PostgresSettings
-from get_weather.weather import Weather
-from get_weather.get_weather import get_weather_from_api, MyModel
